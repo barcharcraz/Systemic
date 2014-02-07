fb519915cfbb0c035a1065fc0cab4ab45efd1938
Charlie Barto <charlie@westquad-148080.reshall.umich.edu>
2014-02-06 19:08:44 -0500

    MakeComponent macro works

--- ecs/SceneNode.nim
+++ ecs/SceneNode.nim
@@ -3,12 +3,16 @@ import macros
 type TSceneNode*[T] = object
   sceneList: seq[seq[T]]
 
-template MakeComponent(typ: expr) {.immediate.} =
-  var `typ SceneNode`: TSceneNode[typ]
-  #echo repr(`typ SceneNode`)
-  #proc addComponent*(scene: TScene, comp: typ) = 
-  #  node.sceneList[scene.id].add(comp)
+macro MakeComponent(typ: expr): expr =
+  var brackets = newNimNode(nnkBracketExpr)
+  brackets.add(newIdentNode(!"TSceneNode"))
+  brackets.add(newIdentNode(!($typ)))
+  var identDefs = newNimNode(nnkIdentDefs)
+  identDefs.add(newIdentNode($typ & "SceneNode"))
+  identDefs.add(brackets)
+  identDefs.add(newNimNode(nnkEmpty))
+  result = newNimNode(nnkVarSection).add(identDefs)
 
 when isMainModule:
   MakeComponent(int)
-  #echo intSceneNode.sceneList.len
+  echo intSceneNode.sceneList.len
